78c04655a559375972ceba1a839ad82a7666c740
christianadianto <christian_adianto@yahoo.com>
2017-06-21 10:50:21 +0700

    upload pertama

---
+++ app/src/main/java/edu/bluejack16_2/cariprojek/MainActivity.java
@@ -0,0 +1,39 @@
+package edu.bluejack16_2.cariprojek;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    Button btnRegis;
+    Button btnExit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnRegis = (Button) findViewById(R.id.btnRegister);
+        btnExit = (Button) findViewById(R.id.btnExit);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/edu/bluejack16_2/cariprojek/RegisterActivity.java
@@ -0,0 +1,79 @@
+package edu.bluejack16_2.cariprojek;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+    Button btnRegis;
+    EditText txtEmail;
+    EditText txtPassword;
+    EditText txtName;
+    EditText txtAddress;
+    EditText txtPhone;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        btnRegis = (Button) findViewById(R.id.btnSubmitRegis);
+        txtEmail = (EditText) findViewById(R.id.txtEmailRegis);
+        txtPassword = (EditText) findViewById(R.id.txtPasswordRegis);
+        txtName = (EditText) findViewById(R.id.txtNameRegis);
+        txtAddress = (EditText) findViewById(R.id.txtAddressRegis);
+        txtPhone = (EditText) findViewById(R.id.txtPhoneRegis);
+
+
+
+        btnRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email="";
+                String password = "";
+                String name = "";
+                String address = "";
+                String phone = "";
+                String err="";
+                email = txtEmail.getText().toString();
+                password = txtPassword.getText().toString();
+                name = txtName.getText().toString();
+                address = txtAddress.getText().toString();
+                phone = txtPhone.getText().toString();
+
+               if(email.equals("")){
+                   err = "email must be filled and valid";
+                   Toast.makeText(RegisterActivity.this, err, Toast.LENGTH_SHORT).show();
+                   return;
+               }
+               if(password.equals("")){
+                   err="password must be filled";
+                   Toast.makeText(RegisterActivity.this, err, Toast.LENGTH_SHORT).show();
+                   return;
+               }
+               if(name.equals("")){
+                   err="name must be filled";
+                   Toast.makeText(RegisterActivity.this, err, Toast.LENGTH_SHORT).show();
+                   return;
+               }
+               if(address.equals("")){
+                   err="address must be filled";
+                   Toast.makeText(RegisterActivity.this, err, Toast.LENGTH_SHORT).show();
+                   return;
+               }
+               if(phone.equals("")){
+                   err="phone must be filled";
+                   Toast.makeText(RegisterActivity.this, err, Toast.LENGTH_SHORT).show();
+                   return;
+               }
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            }
+        });
+    }
+}
