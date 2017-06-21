a81aa29977b11d51c5533ef4bae542911ca4d595
christianadianto <christian_adianto@yahoo.com>
2017-06-21 15:12:44 +0700

    btnExit

--- app/src/main/java/edu/bluejack16_2/cariprojek/MainActivity.java
+++ app/src/main/java/edu/bluejack16_2/cariprojek/MainActivity.java
@@ -1,6 +1,7 @@
 package edu.bluejack16_2.cariprojek;
 
 import android.content.Intent;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -33,6 +34,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(),RegisterActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
             }
         });
     }
